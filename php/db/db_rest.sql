b8629c2a8dd883fa260c5e29519e28ed3401a4da
ThiagoFrederico <thiagofrederico3@gmail.com>
2020-07-21 10:23:30 -0300

    bd atualizado

--- php/db/db_rest.sql
+++ php/db/db_rest.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3308
--- Tempo de geração: 15-Jul-2020 às 11:17
+-- Tempo de geração: 21-Jul-2020 às 13:19
 -- Versão do servidor: 8.0.18
 -- versão do PHP: 7.3.12
 
@@ -21,6 +21,8 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `db_rest`
 --
+CREATE DATABASE IF NOT EXISTS `db_rest` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `db_rest`;
 
 -- --------------------------------------------------------
 
@@ -30,45 +32,37 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `tb_clientes`;
 CREATE TABLE IF NOT EXISTS `tb_clientes` (
-  `cod` int(11) NOT NULL AUTO_INCREMENT,
+  `codCliente` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(60) NOT NULL,
   `email` varchar(50) NOT NULL,
   `cpf` bigint(11) NOT NULL,
   `telefone` bigint(15) NOT NULL,
   `telefone2` bigint(15) NOT NULL,
   `senha` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  PRIMARY KEY (`cod`)
-) ENGINE=MyISAM AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`codCliente`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Extraindo dados da tabela `tb_clientes`
 --
 
-INSERT INTO `tb_clientes` (`cod`, `nome`, `email`, `cpf`, `telefone`, `telefone2`, `senha`) VALUES
-(1, 'UsuarioExemplo', 'UsuarioExemplo@email.com', 11111111, 1122222222, 1133333333, '321'),
+INSERT INTO `tb_clientes` (`codCliente`, `nome`, `email`, `cpf`, `telefone`, `telefone2`, `senha`) VALUES
 (21, 'Vitor Pereira', 'vitor-per@hotmail.com', 50576009822, 11222222222, 11333333333, 'b17f831f6881a76161a5c06a94473a6b'),
 (22, 'Thiago Frederico ', 'thiagofrederico3@gmail.com', 45484515135, 40028922, 3698745, 'e10adc3949ba59abbe56e057f20f883e');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `tb_mesas`
+-- Estrutura da tabela `tb_pratopedido`
 --
 
-DROP TABLE IF EXISTS `tb_mesas`;
-CREATE TABLE IF NOT EXISTS `tb_mesas` (
-  `IdMesa` int(10) NOT NULL,
-  `qtdAcentos` int(20) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Extraindo dados da tabela `tb_mesas`
---
-
-INSERT INTO `tb_mesas` (`IdMesa`, `qtdAcentos`) VALUES
-(1, 5),
-(2, 2),
-(3, 2);
+DROP TABLE IF EXISTS `tb_pratopedido`;
+CREATE TABLE IF NOT EXISTS `tb_pratopedido` (
+  `idReserva` int(100) NOT NULL,
+  `idComida` int(11) NOT NULL,
+  KEY `idComida_fk` (`idComida`),
+  KEY `idReserva_fk` (`idReserva`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -81,7 +75,7 @@ CREATE TABLE IF NOT EXISTS `tb_pratos` (
   `idComida` int(50) NOT NULL AUTO_INCREMENT,
   `comida` varchar(100) NOT NULL,
   PRIMARY KEY (`idComida`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Extraindo dados da tabela `tb_pratos`
@@ -99,20 +93,33 @@ INSERT INTO `tb_pratos` (`idComida`, `comida`) VALUES
 
 DROP TABLE IF EXISTS `tb_reserva`;
 CREATE TABLE IF NOT EXISTS `tb_reserva` (
+  `idReserva` int(100) NOT NULL AUTO_INCREMENT,
   `IdMesa` int(10) NOT NULL,
-  `clienteID` int(10) NOT NULL,
+  `codCliente` int(11) NOT NULL,
   `nomeCliente` varchar(40) NOT NULL,
   `hora` time NOT NULL,
   `data` date NOT NULL,
-  `status` varchar(10) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Registro das Reservas';
+  `status` varchar(10) NOT NULL,
+  PRIMARY KEY (`idReserva`),
+  KEY `codCliente` (`codCliente`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Registro das Reservas';
 
 --
--- Extraindo dados da tabela `tb_reserva`
+-- Restrições para despejos de tabelas
 --
 
-INSERT INTO `tb_reserva` (`IdMesa`, `clienteID`, `nomeCliente`, `hora`, `data`, `status`) VALUES
-(1, 11, 'Vitor P', '10:00:00', '2020-07-13', 'Pedente');
+--
+-- Limitadores para a tabela `tb_pratopedido`
+--
+ALTER TABLE `tb_pratopedido`
+  ADD CONSTRAINT `idComida_fk` FOREIGN KEY (`idComida`) REFERENCES `tb_pratos` (`idComida`),
+  ADD CONSTRAINT `idReserva_fk` FOREIGN KEY (`idReserva`) REFERENCES `tb_reserva` (`idReserva`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Limitadores para a tabela `tb_reserva`
+--
+ALTER TABLE `tb_reserva`
+  ADD CONSTRAINT `codCliente_fk` FOREIGN KEY (`codCliente`) REFERENCES `tb_clientes` (`codCliente`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
